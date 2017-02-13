01949e6f34ccfcd155d8e1fb16b75885722b890e
julien <cohen_p@epita.fr>
2017-02-13 20:48:51 +0100

    [JAVA_WEB] add push down and scroll up on feed news

--- newsReader/www/js/controllers.js
+++ newsReader/www/js/controllers.js
@@ -49,13 +49,35 @@ angular.module('starter.controllers', [])
 })
 
 .controller('DashCtrl', function($http, $scope, $stateParams , Profiles) {
-	$scope.profiles = Profiles.all();
   $scope.stories = [];
-  $http.get("https://www.reddit.com/r/funny/new.json")
-  .success(function (response) {
-    angular.forEach(response.data.children,function (child) {
-      $scope.stories.push(child.data);  
+  function loadStories(params,callback) {
+    $http.get("https://www.reddit.com/r/funny/new.json",{params: params})
+    .success(function (response) {
+      var stories = [];
+      angular.forEach(response.data.children,function (child) {
+        stories.push(child.data);
+      });
+      callback(stories);
     });
-  });
+  }
+   
+  $scope.loadOlderStories = function () {
+    var params ={};
+    if($scope.stories.length > 0){
+      params['after'] = $scope.stories[$scope.stories.length-1].name;
+    }
+    loadStories(params,function (olderStories) {
+      $scope.stories = $scope.stories.concat(olderStories);
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
+  };
+   
+  $scope.loadNewerStories = function () {
+    var params = {'before':$scope.stories[0].name};
+    loadStories(params,function (newerStories) {
+      $scope.stories = newerStories.concat($scope.stories);
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
 });
 
