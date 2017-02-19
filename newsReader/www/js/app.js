5aa184cf1be0e9b0ac97304a09a9ceac55dfcec8
Sami Gomez <supersami@MacBook-Air-de-Sami.local>
2017-02-19 23:55:04 +0100

    Setting page: fix date/hours value

--- newsReader/www/js/app.js
+++ newsReader/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services',
-  'jett.ionic.filter.bar'])
+  'jett.ionic.filter.bar', "ion-datetime-picker"])
 
 .run(function($ionicPlatform , $rootScope, $timeout) {
   $ionicPlatform.ready(function() {
